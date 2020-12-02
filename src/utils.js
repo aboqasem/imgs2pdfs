6bee02ef98585de15d6d90744189b22d69c945c7
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-02 12:43:06 +0800

    Finish filterText & wrapText methods

--- src/utils.js
+++ src/utils.js
@@ -1,3 +1,5 @@
+const { lineSplit } = require("pdf-lib");
+
 /**
  * @param {String} text Original text.
  * @param {Number[]} characterSet Accepted character set.
@@ -8,7 +10,13 @@ function filterText(text, characterSet) {
         return '';
     }
 
-    return text;
+    let filteredText = '';
+    for (let i = 0; i < text.length; i++) {
+        if (characterSet.includes(text.charCodeAt(i))) {
+            filteredText += text.charAt(i);
+        }
+    }
+    return filteredText;
 }
 
 /**
@@ -21,7 +29,22 @@ function wrapText(text, lineLength) {
         return '';
     }
 
-    return text;
+    const lines = [];
+    let line = '';
+    text.split(/\s+/).forEach((word, index, words) => {
+        // accept new words
+        line += `${word} `;
+        // maybe 15 is good to prevent next words from getting out of wrapping bound
+        if (line.length >= lineLength - 15) {
+            lines.push(line.trimEnd());
+            // empty the line when out of safe bound
+            line = '';
+        } else if (index == words.length - 1) {
+            lines.push(line.trimEnd());
+        }
+    });
+
+    return lines.join('\n');
 }
 
 module.exports = { filterText, wrapText };
