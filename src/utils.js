e521a5e2fc86949cca79096e5adfc9e9931900ab
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-02 11:40:20 +0800

    Fix documentation

--- src/utils.js
+++ src/utils.js
@@ -1,6 +1,7 @@
 /**
- * @param {String} text 
- * @param {Number[]} characterSet 
+ * @param {String} text Original text.
+ * @param {Number[]} characterSet Accepted character set.
+ * @returns {String} Filtered string with only the accepted characters, empty string on invalid args.
  */
 function filterText(text, characterSet) {
     if (!(text && characterSet && characterSet.length)) {
@@ -11,10 +12,14 @@ function filterText(text, characterSet) {
 }
 
 /**
- * @param {String} text 
- * @param {Number} lineLength 
+ * @param {String} text Original text.
+ * @param {Number} lineLength Number of characters until wrap.
+ * @returns {String} Wrapped string to th line length, empty string on invalid args.
  */
 function wrapText(text, lineLength) {
+    if (lineLength < 0) {
+        return '';
+    }
 
     return text;
 }
