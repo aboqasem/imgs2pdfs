0bbabb17e6f32851de35e6fb7f70e64b14d6d96c
Mohammad Al Zouabi <62098043+aboqasem@users.noreply.github.com>
2020-12-02 15:33:54 +0800

    Add mapping method from RecognitionResults to PdfData

--- src/pdf/pdf-data-helper.js
+++ src/pdf/pdf-data-helper.js
@@ -5,3 +5,32 @@
  * @typedef {{pdfName: String, imgsData: {imgPath: String, imgText: String}[]}[]} PdfData
  */
 
+
+/**s
+ * @param {import('../ocr/ocr-handler').RecognitionResults} recognitionResults Recognition results to map PDF data from.
+ * @returns {PdfData} Mapped PDF data from container recognition results, empty PdfData on invalid args.
+ */
+function fromRecognitionResults(recognitionResults) {
+    //
+    if (!(recognitionResults && recognitionResults.length)) {
+        console.error('Error: No recognition results to recognize.\n');
+        return [{ pdfName: '', imgsData: [{ imgPath: '', imgText: '' }] }];
+    }
+    // map the PDF data with the available valid recognition results
+    return recognitionResults.map(({ dirName, imgFilesPaths, results }) => {
+        const pdfName = dirName;
+
+        // map the images data with the available data
+        const imgsData = imgFilesPaths.map((imgFilePath, index) => {
+            return {
+                imgPath: imgFilePath,
+                imgText: results[index].data.text,
+            };
+        });
+
+        return {
+            pdfName,
+            imgsData,
+        };
+    });
+}
